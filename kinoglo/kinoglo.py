0d6477987b10e0c05a65e0fcddecb94714900854
DirkHardware <email.the.anderson@gmail.com>
2021-09-12 19:16:06 -0400

    creates a (improperly sized) scrollable treeview window

--- kinoglo/kinoglo.py
+++ kinoglo/kinoglo.py
@@ -12,6 +12,7 @@ class TreeViewFilterWindow(Gtk.Window):
         # Setting up the self.grind in which the elements are to be positioned
         self.grid = Gtk.Grid()
         self.grid.set_row_homogeneous(True)
+        self.grid.set_column_homogeneous(True)
         self.add(self.grid)
 
         # Creating the ListStore model 
@@ -25,13 +26,43 @@ class TreeViewFilterWindow(Gtk.Window):
             abs_path = os.path.join(self.root,dirs)
             if os.path.isdir(os.path.join(abs_path)):
                 self.dir_list.append((dirs, os.path.getsize(abs_path)))
+        print(self.dir_list)
+        for movie_dir in self.dir_list:
+            self.movie_liststore.append(list(movie_dir))
         self.current_filter = None 
-        self.directory_liststore = Gtk.ListStore(str, int)
+        
+        # Creating the filter, feeding it with the ListStore model 
+        self.movie_filter = self.movie_liststore.filter_new()
+        # setting the filter function
+        self.movie_filter.set_visible_func(self.movie_filter_func)
+
+        # creating the treview, making it use the filter as a model and 
+        # adding the columns (the filter is passed as an argument to treeview)
+
+        self.treeview = Gtk.TreeView(model=self.movie_filter)
+        for i, column_title in enumerate(
+                ["Title", "Size"]
+        ):
+            renderer = Gtk.CellRendererText()
+            column = Gtk.TreeViewColumn(column_title, renderer, text=i)
+            self.treeview.append_column(column)
+
+        # setting up the layout, putting the treeview in a scroll window and the buttons in a row
+        # There's something about the presence of the buttons in the tutorial
+        # that makes the treeview size properly
+        self.scrollable_treelist = Gtk.ScrolledWindow()
+        self.scrollable_treelist.set_vexpand(True)
+        self.grid.attach(self.scrollable_treelist, 0, 0, 8, 10)
+        self.scrollable_treelist.add(self.treeview)
+
+        self.show_all()
+
+    def movie_filter_func(self, model, iter, data):
+        return True
+
 
 win = TreeViewFilterWindow()
-print(win.dir_list)
-#root = "/home/anderson"
-#for dirs in os.listdir(root):
-#    abs_path = os.path.join(root,dirs)
-#    if os.path.isdir(abs_path):
-#        print(f"Directory :{dirs} {os.path.getsize(abs_path)}")
+print(win.movie_liststore)
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
