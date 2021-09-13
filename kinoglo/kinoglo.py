8c2c5b4ab3dafcbe78129d01c7345780e6b4b15d
DirkHardware <email.the.anderson@gmail.com>
2021-09-13 17:09:16 -0400

    extracts movie data from JSON files

--- kinoglo/kinoglo.py
+++ kinoglo/kinoglo.py
@@ -1,4 +1,6 @@
-import gi, os
+import gi
+import os
+import json
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
@@ -16,7 +18,7 @@ class TreeViewFilterWindow(Gtk.Window):
         self.add(self.grid)
 
         # Creating the ListStore model 
-        self.movie_liststore = Gtk.ListStore(str, int)
+        self.movie_liststore = Gtk.ListStore(str, str, int, str, str, str, int)
         self.root = "/home/anderson/Videos"
         self.dir_list = []
         # Checks all the files in self.root to determine if they are directories,
@@ -25,7 +27,20 @@ class TreeViewFilterWindow(Gtk.Window):
             # Makes an absolute path out of every directory in self.root
             abs_path = os.path.join(self.root,dirs)
             if os.path.isdir(os.path.join(abs_path)):
-                self.dir_list.append((dirs, os.path.getsize(abs_path)))
+                info_list = list()
+                f = open('{}/data.json'.format(abs_path))
+                data = json.load(f)
+                for i in data['movie_details'][0].items():
+                    if i[0] == "year":
+                        info_list.append(int(i[1]))
+                    else:
+                        info_list.append(i[1])
+                info_list.append(dirs) 
+                info_list.append(os.path.getsize(abs_path))
+                info_tup = tuple(info_list)
+                self.dir_list.append(info_tup)
+                print(info_tup)
+                # self.dir_list.append((dirs, os.path.getsize(abs_path)))
         print(self.dir_list)
         for movie_dir in self.dir_list:
             self.movie_liststore.append(list(movie_dir))
@@ -41,19 +56,44 @@ class TreeViewFilterWindow(Gtk.Window):
 
         self.treeview = Gtk.TreeView(model=self.movie_filter)
         for i, column_title in enumerate(
-                ["Title", "Size"]
+                ["Title", "Director", "Year", "Country", "Genre", "Size"]
         ):
             renderer = Gtk.CellRendererText()
             column = Gtk.TreeViewColumn(column_title, renderer, text=i)
             self.treeview.append_column(column)
-
+       
+        self.buttons = list()
+        for prog_language in ["Java", "C", "C++", "Python", "None"]:
+            button = Gtk.Button(label=prog_language)
+            self.buttons.append(button)
+            button.connect("clicked", self.on_selection_button_clicked)
+        
         # setting up the layout, putting the treeview in a scroll window and the buttons in a row
         # There's something about the presence of the buttons in the tutorial
         # that makes the treeview size properly
+        
         self.scrollable_treelist = Gtk.ScrolledWindow()
         self.scrollable_treelist.set_vexpand(True)
         self.grid.attach(self.scrollable_treelist, 0, 0, 8, 10)
+        self.grid.attach_next_to(
+        self.buttons[0], self.scrollable_treelist, Gtk.PositionType.BOTTOM, 1, 1)
+
+        for i, button in enumerate(self.buttons[1:]):
+            self.grid.attach_next_to(
+                button, self.buttons[i], Gtk.PositionType.RIGHT, 1, 1)
         self.scrollable_treelist.add(self.treeview)
+        
+        # Add menubar
+        self.menubar = Gtk.MenuBar()
+        self.menubar.set_hexpand(True)
+        self.menubar.set_vexpand(True)
+
+        self.menu_file = Gtk.MenuItem('File')
+        self.menubar.append(self.menu_file)
+        
+        self.grid.attach_next_to(self.menubar, self.scrollable_treelist, Gtk.PositionType.TOP, 2, 1)
+        #menu_preferences = self.submenu(menubar, 'Preferences')
+        #menu_filter = self.add_submenu(menubar, 'Filter')
 
         self.show_all()
 
@@ -61,8 +101,10 @@ class TreeViewFilterWindow(Gtk.Window):
         return True
 
 
+    def on_selection_button_clicked(self, widget):
+        pass
+
 win = TreeViewFilterWindow()
-print(win.movie_liststore)
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
 Gtk.main()
